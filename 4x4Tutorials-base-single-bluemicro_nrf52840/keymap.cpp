ac0fa23e97c9f391e767767a749e13b636333a00
Rthrall <124537911+Rthrall@users.noreply.github.com>
2023-05-16 15:39:21 -0700

    Update keymap.cpp

--- 4x4Tutorials-base-single-bluemicro_nrf52840/keymap.cpp
+++ 4x4Tutorials-base-single-bluemicro_nrf52840/keymap.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2021 <Pierre Constantineau>
+Copyright 2018 <Pierre Constantineau>
 
 3-Clause BSD License
 
@@ -7,45 +7,50 @@ Redistribution and use in source and binary forms, with or without modification,
 
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other material>
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior w>
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF >
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN >
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 #include "keymap.h"
 
-// Initialize matrix with nothing...
+
+#if KEYBOARD_SIDE == SINGLE
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {{
-        {LAYER_1,    KC_KP_SLASH,    KC_KP_ASTERISK,     KC_KP_MINUS,},    
-        {KC_KP_7,       KC_KP_8,        KC_KP_9,            KC_KP_PLUS,},
-        {KC_KP_4,       KC_KP_5,        KC_KP_6,            KC_NO,},  
-        {KC_KP_4,       KC_KP_5,        KC_KP_6,            KC_NO,},
-        {KC_DOT,        KC_KP_0,        KC_COLN,            KC_KP_ENTER}
-    }};
+    {KEYMAP(
+        LAYER_1,       KC_KP_SLASH,    KC_KP_ASTERISK,     KC_KP_MINUS,  
+        KC_KP_7,       KC_KP_8,        KC_KP_9,            KC_KP_PLUS,  
+        KC_KP_4,       KC_KP_5,        KC_KP_6,            KC_NO,  
+        KC_KP_1,       KC_KP_2,        KC_KP_3,            KC_NO,  
+        KC_DOT,        KC_KP_0,        KC_COLN,            KC_KP_ENTER 
+    )};
 
 
 void setupKeymap() {
-    
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-            KEYMAP(
-        PRINT_BATTERY,    RESET,    KC_NO,    KC_NO, 
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,   
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    
-        KC_NO,    KC_NO,    KC_NO,    KC_NO );
-    /*
-     * add the other layers
-     */
+
+uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP( \
+  PRINT_BATTERY, RESET,   KC_NO,   KC_NO, \
+  KC_NO,         KC_NO,   KC_NO,   KC_NO,  \
+  KC_NO,         KC_NO,   KC_NO,   KC_NO,    \
+  KC_NO,         KC_NO,   KC_NO,   KC_NO,    \
+  KC_NO,         KC_NO,   KC_NO,   KC_NO   \
+);
+
     for (int row = 0; row < MATRIX_ROWS; ++row)
     {
         for (int col = 0; col < MATRIX_COLS; ++col)
         {
             matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
+            matrix[row][col].addActivation(_L2, Method::PRESS, layer2[row][col]);
+            matrix[row][col].addActivation(_L3, Method::PRESS, layer3[row][col]);
         }
     }
 
+    // if you want to add Tap/Hold or Tap/Doubletap activations, then you add them below.
+
 }
+#endif
